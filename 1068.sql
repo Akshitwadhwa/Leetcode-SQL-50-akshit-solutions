8de1310f5a2e896aa4b66e5c47b3978d8e37f749
akshit_wadhwa <akshitwadhwa06@gmail.com>
2025-02-02 16:26:51 +0530

    Create 1068.sql

---
+++ 1068.sql
@@ -0,0 +1,7 @@
+1068 Product sales analysis
+
+# Write your MySQL query statement below
+select pt.product_name, st.year , st.price
+from Sales st
+inner join Product pt
+on st.product_id = pt.product_id
